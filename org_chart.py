d7c3e14c62f40d301206e81b3814c5918db082b6
EthanSharp512 <ethansharpdev@gmail.com>
2022-07-07 01:48:52 -0600

    org chart file created

---
+++ org_chart.py
@@ -0,0 +1,54 @@
+class Node():
+    """Node in a tree."""
+
+    def __init__(self, data, children):
+        self.data = data
+        self.children = children
+
+    def __repr__(self):
+        """Reader-friendly representation."""
+
+        return f"<Node {self.data}>"
+
+    def find(self, data):
+        """Return node object with this data.
+
+        Start here. Return None if not found.
+        """
+
+        to_visit = [self]
+
+        while to_visit:
+            current = to_visit.pop()
+
+            if current.data == data:
+                return current
+
+            to_visit.extend(current.children)
+
+
+class Tree():
+    """Tree."""
+
+    def __init__(self, root):
+        self.root = root
+
+    def __repr__(self):
+        """Reader-friendly representation."""
+
+        return f"<Tree root={self.root}>"
+
+    def find_in_tree(self, data):
+        """Return node object with this data.
+
+        Start at root.
+        Return None if not found.
+        """
+
+        return self.root.find(data)
+
+
+def make_tree(ceo, direct_reports_list):
+    
+    node = Node(ceo, direct_reports_list)
+    return Tree(node)
